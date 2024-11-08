b01fdfa2e9c27228a72ff9ea6ec0fc59f304cc46
Dag Frode Solberg <dag.solberg@sparebank1.no>
2024-11-08 08:40:13 +0100

    Add more components

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # OlgaBank1
 
 Del av et hackaton hvor vi prøver å lage reimaginere hva en nettbank for eldre kan se ut som.
+
+## TODO
+
+- wrapp local storage i noe hvor en kan gjøre
+  - get put post og delete calls
+  - default verdier hvis en ikke har verdier der til å begynne med
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
+import { Accordion, AccordionItem } from "@sb1/ffe-accordion-react";
 import {
   ActionButton,
   ButtonGroup,
   PrimaryButton,
   ShortcutButton,
 } from "@sb1/ffe-buttons-react";
-import { Heading1 } from "@sb1/ffe-core-react";
+import { IconCard } from "@sb1/ffe-cards-react";
+import { Heading1, Wave } from "@sb1/ffe-core-react";
+import { Datepicker } from "@sb1/ffe-datepicker-react";
+import { Icon } from "@sb1/ffe-icons-react";
+
+import house from "@sb1/ffe-icons/icons/filled/md/60fps.svg";
+import { SearchableDropdown } from "@sb1/ffe-searchable-dropdown-react";
+import { Spinner } from "@sb1/ffe-spinner-react";
 
 export function Index() {
   return (
     <>
       <Heading1>OlgaBank1</Heading1>
+      <Wave color="fjell" />
       <ButtonGroup ariaLabel={""}>
         <ActionButton>Test</ActionButton>
         <PrimaryButton>Test 2</PrimaryButton>
@@ -17,6 +26,42 @@ export function Index() {
           Side 2
         </ShortcutButton>
       </ButtonGroup>
+      <Accordion headingLevel={2}>
+        <AccordionItem heading="Tittel">Skjult innhold</AccordionItem>
+        <AccordionItem heading={<span>Enda en tittel</span>}>
+          Mer skjult innhold
+        </AccordionItem>
+        <AccordionItem heading="En siste tittel">
+          Enda mer innhold
+        </AccordionItem>
+      </Accordion>
+      <IconCard icon={<Icon size="xl" fileUrl={house} />}>
+        {({ CardAction, CardName, Title, Subtext, Text }) => (
+          <>
+            <CardName>Kortnavn</CardName>
+            <Title>
+              <CardAction href="https://design.sparebank1.no">
+                Lenke men hele kortet er klikkbart
+              </CardAction>
+            </Title>
+            <Subtext>En liten undertekst</Subtext>
+            <Text>Her kan man ha tekst</Text>
+          </>
+        )}
+      </IconCard>
+      <Datepicker
+        onChange={function (date: string): void {
+          throw new Error("Function not implemented." + date);
+        }}
+        value={""}
+      />
+      <Spinner />
+      <SearchableDropdown
+        id={""}
+        dropdownList={[]}
+        dropdownAttributes={[]}
+        searchAttributes={[]}
+      />
     </>
   );
 }
