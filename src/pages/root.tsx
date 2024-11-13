57cf8740909e3cb03974850fa95fb3769fd6201d
Anders Fjelleng Engan <anders.fjelleng@sparebank1.no>
2024-11-13 13:45:18 +0100

    Slett sessionstorage og invalidate alle queries når man sletter databasen

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,11 +1,12 @@
 import { ActionButton } from "@sb1/ffe-buttons-react";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
 import { Outlet } from "react-router-dom";
 import { meQuery } from "../queries/me";
 import { Header } from "../router/header";
 
 const Root = () => {
   const { data: me } = useSuspenseQuery(meQuery);
+  const queryClient = useQueryClient();
   return (
     <>
       <Header loggedInUser={me} />
@@ -16,6 +17,8 @@ const Root = () => {
         <ActionButton
           onClick={() => {
             window.indexedDB.deleteDatabase("/pglite/olga-data");
+            window.sessionStorage.clear();
+            queryClient.invalidateQueries();
             window.location.reload();
           }}
         >
