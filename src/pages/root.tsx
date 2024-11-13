b6528a770b76203f98ade1732ab9ce044faf0a5e
Anders Fjelleng Engan <anders.fjelleng@sparebank1.no>
2024-11-13 13:49:50 +0100

    Navigate to front page when resetting

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,12 +1,13 @@
 import { ActionButton } from "@sb1/ffe-buttons-react";
 import { useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
-import { Outlet } from "react-router-dom";
+import { useNavigate, Outlet } from "react-router-dom";
 import { meQuery } from "../queries/me";
 import { Header } from "../router/header";
 
 const Root = () => {
   const { data: me } = useSuspenseQuery(meQuery);
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
   return (
     <>
       <Header loggedInUser={me} />
@@ -19,6 +20,7 @@ const Root = () => {
             window.indexedDB.deleteDatabase("/pglite/olga-data");
             window.sessionStorage.clear();
             queryClient.invalidateQueries();
+            navigate("/");
             window.location.reload();
           }}
         >
