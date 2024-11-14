0b145ce0cbdfcd1fe9fda7db83088f41e4d59984
Anders Fjelleng Engan <anders.fjelleng@sparebank1.no>
2024-11-14 13:03:06 +0100

    Fix link

--- src/pages/(authenticated)/nettbank-privat/dashboard.tsx
+++ src/pages/(authenticated)/nettbank-privat/dashboard.tsx
@@ -12,7 +12,7 @@ const DashBoard = () => {
       <AccountsCard />
       <LatestTransactionsCard />
       <Link
-        to={"./transfer"}
+        to={"./overfore"}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
         Overfør penger
